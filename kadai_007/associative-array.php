9aec3a8c68bc8c2f4e4615ac58b0ccf695a03e74
kazu71054 <158826393+kazu71054@users.noreply.github.com>
2024-06-13 01:03:25 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>課題　連想配列を作って値とキーを出力しよう</title>
+  </head>
+
+  <body>
+    <p>
+      <?php
+      //  連想配列に値を代入する
+        $personal_data=['name' => 'onion' , 'price' => '200' , 'weight' => '160'];
+        
+      // print_r($personal_data);
+        print_r($personal_data);
+      ?>
+    </p>
+  </body>
+</html>
