737a7824536b3b6d06ce5adc6c07a0bf3b0d1bf3
kazu71054 <158826393+kazu71054@users.noreply.github.com>
2024-06-23 11:29:29 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
 <html lang="ja">
+  <head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+  </head>
 
-<head>
-    <meta charset="UTF-8">
-    <title>PHP基礎編</title>
-</head>
+  </head>
 
 <body>
     <p>
         <?php
-        // 数字の代入
-        $nums = array(15,4,18,23,10);
+        $nums = [15,4,18,23,10];
+
+        function sort_2way($array,$order) {
+          if($order){
+            echo "昇順にソートします<br>";
+            asort($array);
+            foreach($array as $key => $val){
+              echo $val . '<br>';
+            }
+          } else {
+            echo"降順にソートします<br>";
+            arsort($array);
+            foreach($array as $key => $val){
+              echo $val . '<br>';
+            }
+          }
+        }
+
+        //表示形式
+        sort_2way($nums, TRUE);
+        sort_2way($nums, FALSE);
         
-        // 昇順と降順それぞれのソート機能の詳細
-        sort($nums, SORT_NATURAL | SORT_FLAG_CASE);
-          // 昇順（TRUE）であれば、「昇順にソートします。」と出力
-          echo "昇順にソートします。<br>";
-
-          // 昇順ソート
-          sort($nums, SORT_NATURAL | SORT_FLAG_CASE);
-          foreach ($nums as $num){
-            echo $num . '<br>';
-          };
-
-          $nums = array(15,4,18,23,10);
-          // 降順（FALSE）であれば、「降順にソートします。」と出力
-            echo  "降順にソートします。<br>";
-            rsort($nums, SORT_NATURAL | SORT_FLAG_CASE);
-            foreach($nums as $num){
-            echo $num . '<br>';
-          };
         ?>
     </p>
 </body>
-
 </html>
